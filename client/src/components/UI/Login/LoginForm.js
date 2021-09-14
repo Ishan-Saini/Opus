51266ecaed4970a12c4321454c44c4a2a96d9eba
Ishan-Saini <ishansaini243@gmail.com>
2021-09-14 22:01:52 +0530

    Add styling in landing page #2

--- client/src/components/UI/Login/LoginForm.js
+++ client/src/components/UI/Login/LoginForm.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import classes from './LoginForm.module.css';
 
 const LoginForm = () => {
   return (
     <form className={classes.loginForm}>
+      <h1>Welcome back!</h1>
       <label for="emailid" className={classes.label}>
-        Email-Id :
+        Email-Id
       </label>
-      <input type="email" className={classes.emailInput} name="emailid" />
+      <input
+        type="email"
+        className={`classes.emailInput + ${classes.inputBox}`}
+        name="emailid"
+      />
       <label for="password" className={classes.label}>
-        Password :
+        Password
       </label>
       <input
         type="password"
-        className={classes.passwordInput}
+        className={`classes.passwordInput + ${classes.inputBox}`}
         name="password"
       />
+      <Link className={classes['reset-link']}>Forgot Password?</Link>
+      <input type="submit" className={classes.submitBtn} />
+      <p className={classes['signup-link']}>Need an account? Signup</p>
     </form>
   );
 };
